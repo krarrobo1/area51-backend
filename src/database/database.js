cd55a5142bd4a483c7c2df2921a129e97e205870
Ricardo <ricardoarrobo@gmail.com>
2019-09-27 11:45:19 -0500

    db

--- src/database/database.js
+++ src/database/database.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
 import { dburi, dbconfig } from '../config/config';
 
-export const sequelize = new Sequelize(dburi, dbconfig);
+//export const sequelize = new Sequelize(dburi, dbconfig);
+export const sequelize = new Sequelize(dburi);
