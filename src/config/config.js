5d7c6078eb061e5f21c11b3c0e0e576bfb16cf49
Ricardo <ricardoarrobo@gmail.com>
2019-10-10 19:54:50 -0500

    quito key

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ export const env = process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 export const dburi = process.env.DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:@localhost:5432/registrate';
 export const seed = process.env.SEED = process.env.SEED || '19wrqk12n3f876653deyuop';
 
-export const google_key = process.env.GOOGLE_KEY || 'AIzaSyBarnFze_ZMmQsDVNOiaDMW75NKzvZA2b4';
+export const google_key = process.env.GOOGLE_KEY;
 
 export const dbconfig = {
     pool: {
