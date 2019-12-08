b91c8e6d01f720542bc898873db4fadc72a4e9a2
Ricardo <ricardoarrobo@gmail.com>
2019-12-07 23:32:48 -0500

    asd

--- src/controllers/asistencia.controller.js
+++ src/controllers/asistencia.controller.js
@@ -81,12 +81,13 @@ export async function registrarAsistenciaWeb(req, res, next) {
             include: [{ model: Empresa, attributes: ['latitud', 'longitud'] }, { model: Cargo, attributes: ['nombre'], include: [{ model: Periodo, attributes: ['horainicio', 'horafin'], include: [{ model: Dia, attributes: ['nombre'] }] }] }]
         });
 
+
         let periodoLaboral = empleado.cargo.periodos;
         // Si esta dentro del periodo puede registrarse...
         if (!comprobarPeriodoLaboral(periodoLaboral)) return res.status(400).json({ ok: false, err: { message: 'FueraDeHorario' } })
 
 
-
+        console.log(JSON.stringify(empleado, null, 2));
         let lastValue = await sequelize.query(`SELECT  * FROM ASISTENCIAS 
         WHERE EMPLEADOID = ${empleado.id}
         order by hora desc 
