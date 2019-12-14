4ff1ac3709c52fd7372d249bb163d22629a4f3bd
Ricardo Arrobo <krac-mac@iMac-de-Ascendere.local>
2019-12-14 17:47:11 -0500

    comentado

--- src/services/redis-client.js
+++ src/services/redis-client.js
@@ -3,11 +3,12 @@ import redis from 'redis';
 
 bluebird.promisifyAll(redis);
 
-
+/*
 const client = redis.createClient({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     //password: process.env.REDIS_PASS || 'password',
 });
 
-export default client;
+
+export default client;*/
