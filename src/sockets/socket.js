8752c8b9858709c195dd0ff0196342c94f113812
Registrate <registrateapp@iMac-de-Ascendere-2.local>
2019-12-18 19:26:28 -0500

    valid

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -26,7 +26,7 @@ io.on('connection', async (client) => {
             await redis.setnxAsync(`${empleadoid}`, connId);
             // Guardamos el id del socket y el del usuario;
             await redis.setnxAsync(connId, data);
-            client.emit('valid', true);
+            client.emit('validado', true);
         }catch(err){
             console.log(`ERROR: ${err}`);
         }
