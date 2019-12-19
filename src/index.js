6687aaeed4608159d92ed8926bf7a6e7554a9f9c
Registrate <registrateapp@iMac-de-Ascendere-2.local>
2019-12-19 18:11:56 -0500

    pingTimeout

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ let server = http.Server(app);
 
 cron.schedule(time, marcarSalidas).start();
 
-let io = socketIO(server, { pingTimeout: 100 });
+let io = socketIO(server, { pingTimeout: 5000 });
 
 
 
