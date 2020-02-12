c220b022a9354d2b67bd7dfb5a196dcf245162b8
RicardoArrobo <ricardoarrobo@gmail.com>
2020-02-12 12:17:41 -0500

    cambio en config

--- src/config/config.js
+++ src/config/config.js
@@ -50,13 +50,7 @@ const prodConfig = {
             require: true
         }
     },
-    pool: {
-        max: 20,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    logging: true,
+    logging: false,
     ssl: true
 }
 
